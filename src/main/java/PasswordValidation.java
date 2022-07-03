887bddb43f531f01c72f9056b0bbb9d0625a4f6f
sarakrieger <sara.krieger2022@gmail.com>
2022-07-03 10:48:15 +0200

    Added first 3 tests

---
+++ src/main/java/PasswordValidation.java
@@ -0,0 +1,21 @@
+public class PasswordValidation {
+
+
+    public static boolean isPasswordMin7Char(String sommer2022) {
+        return false;
+    }
+
+    public static boolean isPasswordLonger7Char(String sommer2022) {
+        return true;
+
+    }
+
+
+    public static boolean hasPasswordDigits(String sommer2022) {
+        return true;
+
+    }
+}
+
+
+
---
+++ src/test/java/PasswordValidationTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidationTest {
+
+    @Test
+    void isPasswordMin7Char() {
+
+        // given
+        boolean letters = false;
+        // when
+        boolean actual = PasswordValidation.isPasswordMin7Char("Sommer2022");
+        // then
+        Assertions.assertEquals(false, actual);
+    }
+
+
+    @Test
+    void isPasswordLonger7Char() {
+
+        //given
+        boolean letters = true;
+        //when
+        boolean actual = PasswordValidation.isPasswordLonger7Char("Sommer2022");
+        //then
+        Assertions.assertEquals(true, actual);
+
+    }
+
+    @Test
+    void hasPasswordDigits() {
+        //given
+        boolean digits = true;
+
+        //when
+        boolean actual = PasswordValidation.hasPasswordDigits("Sommer2022");
+
+        //then
+        Assertions.assertEquals(true, actual);
+    }
+
+}
